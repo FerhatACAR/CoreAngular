38eb5c34ade5f3f7b094b498bf5f8d4aa8385022
FerhatACAR <FerhatACAR1849@yahoo.com>
2023-08-14 23:01:46 +0300

    Dto

--- CoreAngular.API/Controllers/AccountsController.cs
+++ CoreAngular.API/Controllers/AccountsController.cs
@@ -1,8 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using CoreAngular.API.Data;
+using CoreAngular.API.DTOs;
 using CoreAngular.API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreAngular.API.Controllers
 {
@@ -15,13 +17,16 @@ namespace CoreAngular.API.Controllers
         }       
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
+                
+            
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -29,6 +34,11 @@ namespace CoreAngular.API.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
