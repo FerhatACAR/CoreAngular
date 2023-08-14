50d2c1fcff1a18559528797b503dae95345724fa
FerhatACAR <FerhatACAR1849@yahoo.com>
2023-08-15 01:03:54 +0300

    loginDto

--- CoreAngular.API/Controllers/AccountsController.cs
+++ CoreAngular.API/Controllers/AccountsController.cs
@@ -19,8 +19,7 @@ namespace CoreAngular.API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-            if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
-                
+            if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken"); 
             
             using var hmac = new HMACSHA512();
             var user = new AppUser
@@ -32,6 +31,25 @@ namespace CoreAngular.API.Controllers
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
 
             return user;
         }
