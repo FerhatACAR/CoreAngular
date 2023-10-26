6d77f5c02d04fb3c2bb6753566034db8891bdfc3
FerhatACAR <FerhatACAR1849@yahoo.com>
2023-10-27 00:14:43 +0300

    middlewareCreated

--- CoreAngular.API/Program.cs
+++ CoreAngular.API/Program.cs
@@ -1,4 +1,5 @@
 using CoreAngular.API.Extensions;
+using CoreAngular.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,9 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
